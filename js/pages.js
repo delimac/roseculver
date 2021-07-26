c309078f0824e7f0146f7e677cf946390eb5dca4
Chris De Lima <delimac@hey.com>
2021-07-26 16:44:09 +0800

    added prev and next button functions

--- js/pages.js
+++ js/pages.js
@@ -1,6 +1,37 @@
+let pageNumber = 0
+
 const nextTag = document.querySelector("footer img.next")
+const prevTag = document.querySelector("footer img.prev")
 const outputTag = document.querySelector("h2")
 
+const next = function () {
+  pageNumber = pageNumber + 1
+
+  if (pageNumber > 10) {
+    pageNumber = 1
+  }
+
+  updateSection()
+}
+
+const previous = function () {
+  pageNumber = pageNumber - 1
+
+  if (pageNumber < 1) {
+    pageNumber = 10
+  }
+
+  updateSection()
+}
+
+const updateSection = function () {
+  outputTag.innerHTML = pageNumber
+}
+
 nextTag.addEventListener("click", function () {
-  outputTag.innerHTML = "it worked!"
+  next()
+})
+
+prevTag.addEventListener("click", function () {
+  previous()
 })
